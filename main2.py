6cd2f581f98e8e2adc0c0b607f361b078db4d8ac
Laptev Roman <romashlapt@mail.ru>
2024-12-15 15:06:47 +0700

    first commit

---
+++ main2.py
@@ -0,0 +1,90 @@
+from fastapi import FastAPI, Depends, Query, Cookie
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from models import User, Base, Users, CustomException, CountCoffeeCups
+from sqlalchemy import MetaData, create_engine, select, insert, update
+from sqlalchemy.orm import sessionmaker
+from passlib.context import CryptContext
+from typing import Optional
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+metadata = MetaData()
+engine = create_engine("sqlite:///database.db",echo=True)
+sess = sessionmaker(engine)
+
+# Хэширование пароля
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+
+
+def create_tables():
+    engine.echo = False
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    engine.echo = True
+
+        
+create_tables()
+
+@app.get("/register", response_class=HTMLResponse)
+async def register_form(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
+@app.post("/register")
+async def register(user: User = Depends(User.as_form)):
+    hashed_password = pwd_context.hash(user.password)
+    with sess() as session:
+        query = select(Users)
+        res = session.execute(query.where(Users.username == user.username)).scalar_one_or_none()
+        if not res:
+            session.execute(insert(Users), {"username":user.username, "password":hashed_password})
+        else:
+            raise CustomException(detail="Пользователь с таким именем уже существует, попробуйте другое имя", status_code=404)        
+        session.commit()
+
+@app.get("/login", response_class=HTMLResponse)
+async def register_form(request: Request):
+    response = templates.TemplateResponse("login.html", {"request": request})
+    response.set_cookie('kek1', 'lol1')
+    return response
+
+
+
+@app.post("/login")
+async def login(user: User = Depends(User.as_form)):
+    with sess() as session:
+        query = select(Users)
+        res = session.execute(query.where(Users.username == user.username)).scalar_one_or_none()        
+        if not res:
+            raise CustomException(detail="Неверное имя или пароль", status_code=404)        
+        # Проверяем пароль
+        if not pwd_context.verify(user.password, res.password):
+            raise CustomException(detail="Неверное имя или пароль", status_code=404)
+        
+        return {"message": f"Добро пожаловать, {user.username}"}
+
+
+@app.get("/home", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/drink_coffee/" response_class=HTMLResponse)
+async def count_cup(request: Request, jwt):
+    with sess() as session:
+        query = select(CountCoffeeCups)
+        res = session.execute(query.where(id == CountCoffeeCups.user_id)).scalars().all
+        for item in res:
+            count = item.count_cups + 1
+            session.execute(update(CountCoffeeCups).where(id == CountCoffeeCups.id), {"count_cups": count})
+        session.commit()
+    return templates.TemplateResponse(name="home.html", context={"request":request, "count_cups": count})
+
+# def make_jwt_token(user: Users) -> str:
+#     ...
+
+# def get_user_from_jwt_token(jwt_token: str) -> Optional[Users]:
+#     ...
---
+++ models.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from sqlalchemy.orm import sessionmaker, Session, mapped_column, Mapped, DeclarativeBase, relationship
+from fastapi import HTTPException, Form
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
+
+class User(BaseModel):
+    username: str
+    password: str
+
+    @classmethod
+    def as_form(
+        cls,
+        username: str = Form(...),
+        password: str = Form(...),
+    ):
+        return cls(username=username, password=password)
+
+class Base(DeclarativeBase):
+    pass
+
+# пользователи
+class Users(Base):
+    __tablename__ = "users"
+    id: Mapped [int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str]
+    password: Mapped[str]
+
+class CountCoffeeCups(Base):
+    __tablename__ = "coffee_cups"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    count_cups: Mapped[int]
+
+class CustomException(HTTPException):
+    def __init__(self, detail: str, status_code: int = 404):
+        super().__init__(status_code=status_code, detail=detail)
